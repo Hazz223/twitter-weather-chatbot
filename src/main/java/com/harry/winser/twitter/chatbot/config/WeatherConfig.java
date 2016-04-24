39335f81dd9680c6e1b40912a9369ebd88b33864
Harry Winser <harrywinser@hotmail.co.uk>
2016-04-24 17:41:41 +0100

    Added API key to weather. Yet still kinda works without it... Go figure!

--- src/main/java/com/harry/winser/twitter/chatbot/config/WeatherConfig.java
+++ src/main/java/com/harry/winser/twitter/chatbot/config/WeatherConfig.java
@@ -1,15 +1,21 @@
 package com.harry.winser.twitter.chatbot.config;
 
 import org.bitpipeline.lib.owm.OwmClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class WeatherConfig {
 
+    @Value("${open.weather.api.key}")
+    private String apiKey;
+
     @Bean
     public OwmClient getOpenWeatherClient(){
-        return new OwmClient();
+        OwmClient client = new OwmClient();
+        client.setAPPID(apiKey);
+        return client;
     }
 
 }
